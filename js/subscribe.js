cd83d30a9be5dc05e099bc459c4bbe4aeec6fb2f
samueltrillo <knubbe10@gmail.com>
2020-11-01 21:50:11 -0300

    All elements added

--- js/subscribe.js
+++ js/subscribe.js
@@ -1,3 +1,4 @@
+//Elements variable 
 var completeName = document.getElementById("completeName");
 var email = document.getElementById("email");
 var password = document.getElementById("password");
@@ -8,6 +9,14 @@ var adress = document.getElementById("adress");
 var city = document.getElementById("city");
 var zipCode = document.getElementById("zipCode");
 var idCard = document.getElementById("idCard");
+var sendButton = document.getElementById("button");
+//Keydown function for dynamic title
+completeName.addEventListener("keydown", dynamicHello);
+function dynamicHello() {
+   a = document.getElementById("completeName").value;
+   document.getElementById("dynamic-hello").innerHTML = "<h2> Hola "+ a +"</h2>";
+}
+//Blur event
 completeName.addEventListener("blur",veriName);
 email.addEventListener("blur",veriEmail);
 password.addEventListener("blur",veriPass);
@@ -18,84 +27,153 @@ adress.addEventListener("blur",veriAdress);
 city.addEventListener("blur",veriCity);
 zipCode.addEventListener("blur",veriZipCode);
 idCard.addEventListener("blur",veriId);
-
+//Onfocus event
+completeName.addEventListener("focus",adv1);
+email.addEventListener("focus",adv2);
+password.addEventListener("focus",adv3);
+passwordAgain.addEventListener("focus",adv4);
+age.addEventListener("focus",adv5);
+phoneNumber.addEventListener("focus",adv6);
+adress.addEventListener("focus",adv7);
+city.addEventListener("focus",adv8);
+zipCode.addEventListener("focus",adv9);
+idCard.addEventListener("focus",adv10);
+//Onclick function
+sendButton.onclick = function() {
+   alert ("Nombre completo " + veriName() +
+         " - Email " + veriEmail() +
+         " - Contraseña " + veriPass() +
+         " - Repetir contraseña " + veriPass() +
+         " - Edad " + veriAge() +
+         " - Teléfono " + veriPhone() +
+         " - Dirección" + veriAdress() +
+         " - Ciudad " + veriCity() +
+         " - Código Postal " + veriZipCode() +
+         " - DNI " +veriId());
+  
+}
 
 function veriName(){
    a = document.getElementById("completeName").value;
    if (a.indexOf(" ") === -1 || a.length < 7) {
-      alert("Ingrese nombre completo con al menos 6 caracteres y un espacio")
+      document.querySelector(".advName").style.display = "block";
+      return ("Error, revisar condiciones del campo");
    } else {
- 
+      return a;
    }
-  
 }
 function veriEmail() {
    a = document.getElementById("email").value;
    if (a.search(/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/) === -1) {
-      alert("Ingrese una dirección de email correcta");
+      document.querySelector(".advEmail").style.display = "block";
+      return ("Error, revisar condiciones del campo");
+   } else {
+      return a;
    }
 }
 function veriPass() {
    a = document.getElementById("password").value;
    if (/[a-zA-Z]/.test(a) === false || /\s/.test(a) === true || /\d/.test(a) === false || a.length < 8) {
-      alert("ingrese contraseña con letras y numeros");
+      document.querySelector(".advPass").style.display = "block";
+      return ("Error, revisar condiciones del campo");
+   } else {
+      return a;
    }
-
 }
 function veriPassAgain() {
    a = document.getElementById("password").value;
    b = document.getElementById("passwordAgain").value;
    if (a !== b) {
-      alert ("las contraseñas no coinciden");
+      document.querySelector(".advPassAgain").style.display = "block";
+      return ("Error, revisar condiciones del campo");
+   } else {
+      return a;
    }
-
 }
 function veriAge() {
    a = document.getElementById("age").value;
    if (a < 18 || Number.isInteger(Number(a)) === false) {
-      alert("numero no entero");
+      document.querySelector(".advAge").style.display = "block";
+      return ("Error, revisar condiciones del campo");
+   } else {
+      return a;
    }
-
 }
 function veriPhone() {
    a = document.getElementById("phoneNumber").value;
    if ( a.length < 7 || /[a-zA-Z]/.test(a) === true) {
-   alert("telefono no valido");
-
+      document.querySelector(".advPhone").style.display = "block";
+      return ("Error, revisar condiciones del campo");
+   } else {
+      return a;
    }
 }
 function veriAdress() {
    a = document.getElementById("adress").value;
    if ( a.length < 7 || /\s/.test(a) === false || /\d/.test(a) === false || /[a-zA-Z]/.test(a) === false ) {
-      alert ("asd");
+      document.querySelector(".advAdress").style.display = "block";
+      return ("Error, revisar condiciones del campo");
+   } else {
+      return a;
    }
 }
 function veriCity() {
    a = document.getElementById("city").value;
    if (a.length < 3) {
-      alert ("ciudad con al menos 3 caracteres");
-
+      document.querySelector(".advCity").style.display = "block";
+      return ("Error, revisar condiciones del campo");
+   } else {
+      return a;
    }
-
 }
 function veriZipCode(){
    a = document.getElementById("zipCode").value;
    if (a.length < 3) {
-      alert ("ciudad con al menos 3 caracteres");
-
+      document.querySelector(".advZip").style.display = "block";
+      return ("Error, revisar condiciones del campo");
+   } else {
+      return a;
    }
-
 }
 function veriId() {
    a = document.getElementById("idCard").value;
    if (a.length < 7 || a.length > 8) {
-      alert("dni de 7 u 8 digitos");
-
+      document.querySelector(".advIdCard").style.display = "block";
+      return ("Error, revisar condiciones del campo");
+   } else {
+      return a;
    }
-
-
 }
-
+function adv1(){
+   document.querySelector(".advName").style.display = "none";
+}
+function adv2(){
+   document.querySelector(".advEmail").style.display = "none";
+}
+function adv3(){
+   document.querySelector(".advPass").style.display = "none";
+}
+function adv4(){
+   document.querySelector(".advPassAgain").style.display = "none";
+}
+function adv5(){
+   document.querySelector(".advAge").style.display = "none";
+}
+function adv6(){
+   document.querySelector(".advPhone").style.display = "none";
+}
+function adv7(){
+   document.querySelector(".advAdress").style.display = "none";
+}
+function adv8(){
+   document.querySelector(".advCity").style.display = "none";
+}
+function adv9(){
+   document.querySelector(".advZip").style.display = "none";
+}
+function adv10(){
+   document.querySelector(".advIdCard").style.display = "none";
+}
 
 //teElement("div"); //Element div created
 //var text = document.createTextNode("hooolaaa"); //Text created
@@ -103,4 +181,3 @@ function veriId() {
 //var container = document.querySelector(".box1");
 //var ufff = document.querySelector(".completeName");
 //ufff.insertAdjacentElement("afterend",adv);
-
